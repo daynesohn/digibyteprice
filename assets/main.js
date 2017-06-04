f2cb4361fe751b558bf5a7de55a1e023ba6200fe
daynesohn <dayne.sohn@gmail.com>
2017-06-04 12:41:47 -0500

    adding if/else for percentChange variable

--- assets/main.js
+++ assets/main.js
@@ -6,13 +6,18 @@ digiByte.renderPrices = function(response) {
   var $showPrice = $('#dollar');
   var htmlPrice = '<span>$' + currentPrice + '</span>';
 
-  var percentChange = response[0].percent_change_24h;
-  var $showPerChange = $('#percent-change');
-  var htmlPerChange = '<span>' + percentChange + '</span>';
-
   var coinRank = response[0].rank;
   var $showRank = $('#coin-rank');
   var htmlRank = '<span>Coin Rank: ' + coinRank + '</span>';
+
+  //percentage change - adding if statement for green if positive, red if negative
+  var percentChange = response[0].percent_change_24h;
+  var $showPerChange = $('#percent-change');
+    if (percentChange >= 0) {
+    var htmlPerChange = '<span class="green">+' + percentChange + '% over 24h</span>';
+  } else {
+    var htmlPerChange = '<span class="red">' + percentChange + '% over 24h</span>';
+  };
 
   $showPrice.empty().append(htmlPrice);
   $showPerChange.empty().append(htmlPerChange);
