c9f83823f50c091406948ef18ec336d8d05f67f8
daynesohn <dayne.sohn@gmail.com>
2017-06-05 01:12:16 -0500

    adding chart.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var digiByte = {};
+var response;
 
 digiByte.renderCoin = function(response) {
 
@@ -43,6 +44,57 @@ digiByte.renderPolo = function(response) {
 
 }
 
+//chart.js implementation
+function drawLineChart(response) {
+
+  // Add a helper to format timestamp data
+  Date.prototype.formatMMDDYYYY = function() {
+      return (this.getMonth() + 1) +
+      "/" +  this.getDate() +
+      "/" +  this.getFullYear();
+  }
+
+  var jsonData = $.ajax({
+    url: 'https://poloniex.com/public?command=returnChartData&currencyPair=BTC_DGB&start=1405699200&end=9999999999&period=14400',
+    dataType: 'json',
+  }).done(function (results) {
+    // Split timestamp and data into separate arrays
+    var labels = [], data=[];
+
+    for (var labelCount=0; labelCount<35; labelCount++) {
+      labels.push(results[labelCount].date)
+    }
+    for (var dataCount=0; dataCount<35; dataCount++) {
+      data.push(((results[dataCount].open)+(results[dataCount].close)/2)*(response.USDT_BTC.last))
+    }
+
+    // Create the chart.js data structure using 'labels' and 'data'
+    var tempData = {
+      labels : labels,
+      datasets : [{
+          backgroundColor : "#f4d35e",
+          label: 'Average DGB price in USD',
+          strokeColor : "rgba(151,187,205,1)",
+          pointColor : "rgba(151,187,205,1)",
+          pointBackgroundColor : "#99a1aa",
+          pointHighlightFill : "#fff",
+          pointRadius : 6.75,
+          data : data
+      }]
+    };
+
+    // Get the context of the canvas element we want to select
+    var ctx = document.getElementById("myLineChart").getContext("2d");
+
+    // Instantiate a new chart
+    var myNewChart = new Chart(ctx , {
+    type: "line",
+    data: tempData,
+    });
+  });
+}
+
+//get data from coinmarketcap
 var getCoin = function() {
   $.ajax({
     url: 'https://api.coinmarketcap.com/v1/ticker/digibyte/?convert=USD',
@@ -53,19 +105,23 @@ var getCoin = function() {
   })
 };
 
+//get data from poloniex
 var getPolo = function() {
   $.ajax({
     url: 'https://poloniex.com/public?command=returnTicker',
     dataType: 'json',
     success: function(response) {
       digiByte.renderPolo(response);
+      drawLineChart(response);
     }
   })
 };
 
-$(document).ready(getCoin);
-$(document).ready(getPolo)
+//$(document).ready(getCoin);
+$(document).ready(getPolo);
+$(window).load(drawLineChart);
 
 //refresh data every 60 seconds
 //setInterval(getCoin, 60000);
 //setInterval(getPolo, 60000);
+//setInterval(drawLineChart, 60000);
